8120b3000322c40839b400781d9dc6a03401b06e
Maxi0fc <stengardshult92@gmail.com>
2026-03-20 11:31:38 +0100

    Update NotePadWindow.cs

--- NotePadMod/UI/NotePadWindow.cs
+++ NotePadMod/UI/NotePadWindow.cs
@@ -29,7 +29,7 @@ public class NotePadWindow(nint ptr) : MonoBehaviour(ptr)
     private const float TextY = 1f;
     // Offset från knappens position till fönstrets centrum
     private const float WindowOffsetX = -1.5f;
-    private const float WindowOffsetY = 1.2f;
+    private const float WindowOffsetY = 2.0f;
     // ---------------------
     // BepInEx config — text color
     public static ConfigEntry<bool>? ColorBlack;
